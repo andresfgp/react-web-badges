18fec012df4811a92039c962eef34e6476b80474
andresfgp <64049320+andresfgp@users.noreply.github.com>
2021-09-23 02:47:55 -0500

    Updates

---
+++ precache-manifest.d675a05c600fbf7963844e31817cb551.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "307decc6ce4fdbb845e7",
+    "url": "/web-badges/static/css/main.be698970.chunk.css"
+  },
+  {
+    "revision": "307decc6ce4fdbb845e7",
+    "url": "/web-badges/static/js/main.307decc6.chunk.js"
+  },
+  {
+    "revision": "b43403c4060f9fd091ed",
+    "url": "/web-badges/static/js/runtime~main.b43403c4.js"
+  },
+  {
+    "revision": "1d20e32b6ff9e00bd3ea",
+    "url": "/web-badges/static/css/2.7beb5f14.chunk.css"
+  },
+  {
+    "revision": "1d20e32b6ff9e00bd3ea",
+    "url": "/web-badges/static/js/2.1d20e32b.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/web-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "3d74e8bfd4ef7985f7529bb9f7650eca",
+    "url": "/web-badges/static/media/twitter.3d74e8bf.png"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/web-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "9d59b94d59a0956438d9782d77662deb",
+    "url": "/web-badges/index.html"
+  }
+];
